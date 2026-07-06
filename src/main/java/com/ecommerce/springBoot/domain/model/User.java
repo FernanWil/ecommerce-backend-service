5ee0b083d88f5470a839febc3c197dd9859dec6f
FernanWil <fernandowilfre.13@gmail.com>
2026-07-06 16:29:06 -0500

    refactor: implement regrex email validation in user domain

--- src/main/java/com/ecommerce/springBoot/domain/model/User.java
+++ src/main/java/com/ecommerce/springBoot/domain/model/User.java
@@ -1,9 +1,16 @@
 package com.ecommerce.springBoot.domain.model;
 
+import java.util.regex.Pattern;
+
 public record User(
         Integer id,
         String name,
         String email,
         String password
 ) {
+    private static final String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+    public  boolean isValidEmail(String email){
+        if (email == null) return  false;
+        return Pattern.compile(emailRegex).matcher(email).matches();
+    }
 }
