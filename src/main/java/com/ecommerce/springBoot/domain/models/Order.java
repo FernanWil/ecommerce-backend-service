812d4338e90b13cc340f91c4fbf3c34596d7689c
FernanWil <fernandowilfre.13@gmail.com>
2026-07-10 12:18:29 -0500

    correct return type in calulateSubtotal to fix stream reduction

--- src/main/java/com/ecommerce/springBoot/domain/models/Order.java
+++ src/main/java/com/ecommerce/springBoot/domain/models/Order.java
@@ -1,12 +1,23 @@
 package com.ecommerce.springBoot.domain.models;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
-public record Order(String userId) {
-//    public static Order createNew(String name, List<Product> items){
-//        return new Order(
-//                name
-//
-//        );
-//    }
+public record Order(
+        String id,
+        String userId,
+        List<OrderLine> lines,
+        BigDecimal totalPrice
+) {
+
+    public static Order createNew(String userId, List<OrderLine> lines){
+        BigDecimal total = lines.stream().map(OrderLine:: calculateSubtotal).reduce(BigDecimal.ZERO, BigDecimal::add);
+        return new Order(
+            UUID.randomUUID().toString(),
+            userId,
+            lines,
+            total
+        );
+    }
 }
--- src/main/java/com/ecommerce/springBoot/domain/models/OrderLine.java
+++ src/main/java/com/ecommerce/springBoot/domain/models/OrderLine.java
@@ -1,5 +1,16 @@
 package com.ecommerce.springBoot.domain.models;
 
-public record OrderLine (String productId) {
+import java.math.BigDecimal;
 
+public record OrderLine (
+        String Id,
+        String productId,
+        Integer quantity,
+        BigDecimal price
+) {
+
+    public BigDecimal calculateSubtotal() {
+        BigDecimal newStock = BigDecimal.valueOf(this.quantity);
+        return this.price.multiply(newStock);
+    }
 }
