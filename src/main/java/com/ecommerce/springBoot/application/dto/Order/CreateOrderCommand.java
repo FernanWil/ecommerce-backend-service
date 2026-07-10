9624367d0ef02056c57484d7bce81fb1618fe476
FernanWil <fernandowilfre.13@gmail.com>
2026-07-10 13:15:38 -0500

    refactor: add strict input validation to CreateOrderCommand record

--- src/main/java/com/ecommerce/springBoot/application/dto/Order/CreateOrderCommand.java
+++ src/main/java/com/ecommerce/springBoot/application/dto/Order/CreateOrderCommand.java
@@ -1,14 +1,20 @@
 package com.ecommerce.springBoot.application.dto.Order;
 
 import com.ecommerce.springBoot.domain.models.Product;
+
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 public record CreateOrderCommand(
         String userId,
         List<Product> items
 ) {
     public CreateOrderCommand{
-        if(userId == null) throw new IllegalArgumentException("User must not be null");
-        if (items == null || items.isEmpty()) System.out.println("Items can't be null ");
+        if(!Objects.requireNonNull(Boolean.valueOf(userId))) throw new IllegalArgumentException("User must not be null");
+        if (items == null || items.isEmpty()) throw new IllegalArgumentException("Items can't be null ");
+        for(Product item: items) {
+            if (item.price().compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("Price must be greater than 0");
+        }
     }
 }
